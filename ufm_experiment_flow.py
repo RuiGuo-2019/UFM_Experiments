44bae7642fec9fa4bd00e42bfe13dec5ca13bb6b
RuiGuo-2019 <54342033+RuiGuo-2019@users.noreply.github.com>
2023-06-28 00:22:09 -0400

    Add files via upload

--- ufm_experiment_flow.py
+++ ufm_experiment_flow.py
@@ -640,7 +640,8 @@ class ufm_experiment_flow:
             # self.lut_gen1(in_width, out_width)
             # self.lut_mux_gen2(in_width, out_width)
             # self.lut_mux_gen3(in_width, out_width)
-            self.lut_mux_gen4(in_width, out_width)
+            # self.lut_mux_gen4(in_width, out_width)
+            self.lut_mux_gen4_mod(in_width, out_width)
         
         # if(',\n  ' == strRedact[-4:]):
         #     strRedact = strRedact[:-4]
@@ -1051,6 +1052,80 @@ class ufm_experiment_flow:
 
         for OUT in range(out_width):
             f.write("  output reg out{},\n".format(OUT))
+
+
+
+        f.write("  input [{}:0] prog_key\n);".format((2**in_width)*out_width - 1))
+        f.write("\n")
+
+
+
+        o = 0;
+        while(o < out_width):
+            i = 2**in_width;
+            j = 0
+            while i > 1:
+                i = math.ceil(i / 4);
+                f.write("//Iter - {1},{0},{2}\n".format(i, j, o))
+                for l in range(i):
+                    if ((j==0) & (i != 1)):
+                        f.write("MX4X1 mux_{0}_{1}_{8} (out_{0}_{1}_{8}_wire, prog_key[{4}], prog_key[{5}], prog_key[{6}], prog_key[{7}], in{2}, in{3});\n".format(l,j,j*2,j*2+1,(l)*4+(o*(2**in_width)),(l)*4+1+(o*(2**in_width)),(l)*4+2+(o*(2**in_width)),(l)*4+3+(o*(2**in_width)),o));
+                    elif ((j > 0) & (i != 1)):
+                        f.write("MX4X1 mux_{0}_{1}_{9} (out_{0}_{1}_{9}_wire, out_{5}_{4}_{9}_wire, out_{6}_{4}_{9}_wire, out_{7}_{4}_{9}_wire, out_{8}_{4}_{9}_wire, in{2}, in{3});\n".format(l,j,j*2,j*2+1,j-1,4*l,4*l+1,4*l+2,4*l+3,o));
+                    elif ((j > 0) & (i == 1) & (in_width%2 == 0)):
+                        f.write("MX4X1 mux_{0}_{1}_{9} (out{9}, out_{5}_{4}_{9}_wire, out_{6}_{4}_{9}_wire, out_{7}_{4}_{9}_wire, out_{8}_{4}_{9}_wire, in{2}, in{3});\n".format(l,j,j*2,j*2+1,j-1,4*l,4*l+1,4*l+2,4*l+3,o));
+                    elif ((j > 0) & (i == 1) & (in_width%2 == 1)):
+                        f.write("MX2X1 mux_{0}_{1}_{7} (out{7}, out_{5}_{4}_{7}_wire, out_{6}_{4}_{7}_wire, in{2});\n".format(l,j,j*2,j*2+1,j-1,4*l,4*l+1,o));
+                    elif ((j == 0) & (i == 1) & (in_width%2 == 0)):
+                        f.write("MX4X1 mux_{0}_{1}_{8} (out{8}, prog_key[{4}], prog_key[{5}], prog_key[{6}], prog_key[{7}], in{2}, in{3});\n".format(l,j,j*2,j*2+1,4*l+(o*(2**in_width)),4*l+(o*(2**in_width))+1,4*l+(o*(2**in_width))+2,4*l+(o*(2**in_width))+3,o));
+                    elif ((j == 0) & (i == 1) & (in_width%2 == 1)):
+                        f.write("MX2X1 mux_{0}_{1}_{6} (out{6}, prog_key[{4}], prog_key[{5}],, in{2});\n".format(l,j,j*2,j*2+1,4*l+(o*(2**in_width)),4*l+(o*(2**in_width))+1,o));               
+                j = j + 1;
+            o = o + 1;
+
+
+
+        f.write("endmodule")
+        f.close()
+        os.chdir(self.workdir)
+        print("Generate lut_{1}_{0}.v finish".format(out_width,in_width))
+
+    
+    def lut_mux_gen4_mod(self, in_width, out_width): # change 'output reg' to 'output'
+        # !/usr/bin/env python3
+        # import itertools
+        # import math
+        # import matplotlib
+        # import numpy as np
+        # in_width = 1
+        # out_width = 2
+        strTemp = str(in_width) + ' ' + str(out_width)
+        if(strTemp in self.listGeneratedLut):
+            print("lut_{1}_{0}.v already exist.".format(out_width,in_width))
+            return
+        self.listGeneratedLut.append(strTemp)
+        os.chdir(self.strScriptsRoot)
+        # #!/usr/bin/env python3
+        # import itertools
+        # import math
+        # import matplotlib
+        # import numpy as np
+        # in_width = 4
+        # out_width = 2
+        filepath = os.path.join(self.strRTL_LUTRoot, "lut_{1}_{0}.v".format(out_width,in_width))
+        # f = open("./rtl/lut_{1}_{0}.v".format(out_width,in_width), "w")
+        f = open(filepath, "w")
+
+
+
+        f.write("module lut_{1}_{0}(\n".format(out_width,in_width))
+        for IN  in range(in_width):
+            f.write("  input in{},\n".format(IN))
+
+
+
+        for OUT in range(out_width):
+            f.write("  output out{},\n".format(OUT))
 
 
 
